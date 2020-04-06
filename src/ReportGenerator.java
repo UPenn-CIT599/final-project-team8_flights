ae35fbf4956a0b9d803ba2108ac50564cde29721
Viranchi299 <62905727+Viranchi299@users.noreply.github.com>
2020-04-05 22:22:16 -0700

    Add files via upload

---
+++ src/ReportGenerator.java
@@ -0,0 +1,24 @@
+/**The ReportGenerator Class gets data from the Recommender Class, and generates a report 
+ * of the recommended flights for the user
+ */
+public class ReportGenerator {
+	
+	/**Generates a report with the top recommended flights and their booking details,
+	 * and writes to a txt file*/
+	void generateReport() {
+		/*to-do: 
+		 * Write method that retrieves information from the data processing class 
+		 * and generates a text report with recommended flight details
+		 */
+	}
+	
+	/**Draws recommended flight paths on a map of the world, and writes to an image file*/
+	void drawFlightPaths() {
+		/*to-do:
+		 * Write method that takes in the departure and destination
+		 * cities for the recommended flights, and draws out flight
+		 * paths overlaid on a map of the world
+		 */
+	}
+	
+}
